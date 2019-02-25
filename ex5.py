fc5a54a9e7e256d7568812801c03b2b78c2636cb
Alice <38302152+Flowercao@users.noreply.github.com>
2019-02-25 22:09:07 +1300

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Alice Cao'
+my_age = 28 # not a lie
+my_height = 160 #cm
+my_weight = 50#kg
+my_eyes = 'black'
+my_teeth = 'white'
+my_hair = 'black'
+
+print "Let's talk about {my_name}."
+print "she's {my_height} cm tall."
+print "She's {my_weight} kg heavy."
+print 'Actually that is not too heavy.'
+print "She's got {my_eyes} eyes and {my_hair} hair."
+print 'Her teeth are usually {my_teeth} depending on the coffee'
+
+# This line is tricky, try to get it exactly right
+totle = my_age + my_height + my_weight
+print 'If I add {my_age},{my_height},and {my_weight} I get {totle}'
