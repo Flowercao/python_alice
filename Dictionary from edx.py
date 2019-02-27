32b9c2be03806d86fab2cff32ca05ed602469378
Alice <38302152+Flowercao@users.noreply.github.com>
2019-02-27 21:19:45 +1300

    Add files via upload

---
+++ Dictionary from edx.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 18 16:00:56 2018
+
+@author: caohuihui
+"""
+
+#def lyrics_to_frequencies(lyrics):
+#    myDict = {}
+#    for word in lyrics:
+#        if word in myDict:
+#            myDict[word] += 1
+#        else:
+#            myDict[word] = 1
+#    return myDict
+    
+#def most_common_words(freqs):
+#    values = freqs.values()
+#    best = max(values)
+#    words = []
+#    for k in freqs:
+#        if freqs[k] == best:
+#            words.append(k)
+#    return (words, best)
